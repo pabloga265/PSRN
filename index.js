49eed550085508accba0563c84867d9c5f31ff86
Pablo de la Gándara <pabloga265@hotmail.es>
2018-08-29 16:54:19 +0200

    Fucking up

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { Navigation } from 'react-native-navigation'
 import App from './App'
 
-Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App)
+Navigation.registerComponent(`navigation.PSRN.WelcomeScreen`, () => App)
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
       component: {
-        name: 'navigation.playground.WelcomeScreen'
+        name: 'navigation.PSRN.WelcomeScreen'
       }
     }
   })
